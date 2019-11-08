9532d22851ac3d3da5abc759015fc033d3ee6414
Celeste Morton <creneemorton@gmail.com>
2019-11-08 14:38:46 -0800

    display the your pizza will cost text to the user online

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,36 @@
-//Back-end Logic Goes Here:
-
+//Backend Logic:
 function Pizza(size, veggies) {
-    this.size = size,
+    this.movie = size,
         this.veggies = veggies,
         this.price = 0
 }
 
-Pizza.prototype.getPrice = function () {
+Pizza.prototype.addAnswer = function () {
     if (this.size === 20) {
-        this.price += 30;
+        this.price += 30
     } else if (this.size === 16) {
-        this.price += 20;
+        this.price += 20
     } else if (this.size === 12) {
-        this.price += 10;
+        this.price += 10
     }
-
     return this.price;
+
 }
 
-var Pizza = new Pizza();
+var pizza = new Pizza();
 
-//Front-end Logic Goes Here:
 
+//Front end logic:
 $(document).ready(function () {
-    $("form#pizza-pizza").submit(function (event) {
+    $("form#formInput").submit(function (event) {
         event.preventDefault();
-        var sizeInput = parseInt($("input:radio[name=size]:checked").val());
-        var veggiesInput = parseInt($("input:radio[name=veggies]:checked").val());
-        var pizza  = new Ticket (inputMovie, inputAge, inputTime);
-        pizza.getPrice();
-        console.log(pizza.Price);
+        var inputSize = parseInt($("input:radio[name=size]:checked").val());
+        var inputVeggies = parseInt($("input:radio[name=veggies]:checked").val());
+        var pizza = new Pizza(inputSize, inputVeggies);
+        pizza.addAnswer();
+        console.log(pizza.price);
 
-        $("#output").text(pizza.Price);
+        $("#output").html("Your pizza will cost $" + pizza.price);
 
     });
 });
-
-//-------------- jQuery Dom Magic Goes Here ------------------
-// $(document).ready(function () {
-//     $("form#formInput").submit(function (event) {
-//         event.preventDefault();
-//         var inputMovie = parseInt($("input:radio[name=option1]:checked").val());
-//         var inputAge = parseInt($("input:radio[name=option2]:checked").val());
-//         var ticket = new Ticket(inputMovie, inputAge, inputTime);
-//         ticket.addAnswer();
-//         console.log(ticket.price);
-
-//         $("#output").html("Your ticket will cost $" + ticket.price + "! So much money!");
-
-//     });
-// });
