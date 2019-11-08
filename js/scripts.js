280773c341e35de57b3a7fbeb6c89f348a827ff2
Celeste Morton <creneemorton@gmail.com>
2019-11-08 09:21:26 -0800

    add the prototype to the BE logic specifiying the prices assocated with each size

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-//Backend Logic Goes Here:
-
+//Back-end Logic Goes Here:
 
 function Pizza(size, toppings) {
     this.size = size;
@@ -7,8 +6,18 @@ function Pizza(size, toppings) {
     this.price = 0;
 }
 
-Pizza.prototype.getPrice = function() {
-    this.toppings.forEach(function(topping) {
-        this.price =+
+Pizza.prototype.getPrice = function () {
+    this.toppings.forEach(function (toppings) {
+        this.price += 2;
+        if (this.size === 20) {
+            this.price += 30;
+        } else if (this.size === 16) {
+            this.price += 20;
+        } else if (this.size === 12) {
+            this.price += 10;
+        }
+        console.log(this.price);
+
+        return this.price;
     })
 }
